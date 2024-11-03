e6438a4f391668ad5853f18e03ddb01f0de9c4c8
Kaleab-Shewangizaw <kaleab.stk@gmail.com>
2024-11-04 00:16:18 +0300

    created the login and create user functionality

--- api/controllers/auth_controller.js
+++ api/controllers/auth_controller.js
@@ -1,14 +1,39 @@
 import User_models from "../models/user_models.js";
+import bcrypt from "bcryptjs";
+import { createError } from "../utils/error.js";
 
 export const register = async (req, res, next) => {
   try {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
     const newUser = new User_models({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hash,
     });
     await newUser.save();
     res.status(201).send("User registered successfully!");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const user = await User_models.findOne({ username: req.body.username });
+    if (!user) {
+      return next(createError(404, "User not found"));
+    }
+    const isPasswordCorrect = bcrypt.compareSync(
+      req.body.password,
+      user.password
+    );
+    if (!isPasswordCorrect) {
+      return next(createError(400, "Wrong Passoword or username"));
+    }
+    const { password, isAdmin, ...others } = user._doc;
+    res.status(200).json(others);
   } catch (err) {
     next(err);
   }
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { register } from "../controllers/auth_controller.js";
+import { register, login } from "../controllers/auth_controller.js";
 
 const router = express.Router();
 router.post("/register", register);
+router.post("/login", login);
 
 export default router;
